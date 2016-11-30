39b2e0ac8061d790849a2efc109ee6a71f7eb177
mdineshari <mdineshari@gmail.com>
2016-11-30 21:23:54 +0530

    first commit

---
+++ Newzel_Samples/Epub3-Fixed_Layout_Interactive/OEBPS/script/answerp6.js
@@ -0,0 +1,209 @@
+window.onLoad = document.getElementById('w31_1').style.visibility='hidden';
+window.onLoad = document.getElementById('w31_2').style.visibility='hidden';
+window.onLoad = document.getElementById('w31_3').style.visibility='hidden';
+window.onLoad = document.getElementById('w31_4').style.visibility='hidden';
+
+window.onLoad = document.getElementById('w32_1').style.visibility='hidden';
+window.onLoad = document.getElementById('w32_2').style.visibility='hidden';
+window.onLoad = document.getElementById('w32_3').style.visibility='hidden';
+window.onLoad = document.getElementById('w32_4').style.visibility='hidden';
+
+window.onLoad = document.getElementById('w33_1').style.visibility='hidden';
+window.onLoad = document.getElementById('w33_2').style.visibility='hidden';
+window.onLoad = document.getElementById('w33_3').style.visibility='hidden';
+window.onLoad = document.getElementById('w33_4').style.visibility='hidden';
+
+window.onLoad = document.getElementById('w34_1').style.visibility='hidden';
+window.onLoad = document.getElementById('w34_2').style.visibility='hidden';
+window.onLoad = document.getElementById('w34_3').style.visibility='hidden';
+window.onLoad = document.getElementById('w34_4').style.visibility='hidden';
+
+window.onLoad = document.getElementById('w35_1').style.visibility='hidden';
+window.onLoad = document.getElementById('w35_2').style.visibility='hidden';
+window.onLoad = document.getElementById('w35_3').style.visibility='hidden';
+window.onLoad = document.getElementById('w35_4').style.visibility='hidden';
+
+window.onLoad = document.getElementById('w36_1').style.visibility='hidden';
+window.onLoad = document.getElementById('w36_2').style.visibility='hidden';
+window.onLoad = document.getElementById('w36_3').style.visibility='hidden';
+window.onLoad = document.getElementById('w36_4').style.visibility='hidden';
+
+window.onLoad = document.getElementById('w37_1').style.visibility='hidden';
+window.onLoad = document.getElementById('w37_2').style.visibility='hidden';
+window.onLoad = document.getElementById('w37_3').style.visibility='hidden';
+window.onLoad = document.getElementById('w37_4').style.visibility='hidden';
+
+function isOneChecked31_1()
+{	document.getElementById('w31_1').style.visibility='visible';
+	document.getElementById('w31_2').style.visibility='hidden'; 
+	document.getElementById('w31_3').style.visibility='hidden';
+	document.getElementById('w31_4').style.visibility='hidden';
+}
+function isOneChecked31_2()
+{	document.getElementById('w31_1').style.visibility='hidden';
+	document.getElementById('w31_2').style.visibility='visible'; 
+	document.getElementById('w31_3').style.visibility='hidden';
+	document.getElementById('w31_4').style.visibility='hidden';
+}
+function isOneChecked31_3()
+{	document.getElementById('w31_1').style.visibility='hidden';
+	document.getElementById('w31_2').style.visibility='hidden'; 
+	document.getElementById('w31_3').style.visibility='visible';
+	document.getElementById('w31_4').style.visibility='hidden';
+}
+function isOneChecked31_4()
+{	document.getElementById('w31_1').style.visibility='hidden';
+	document.getElementById('w31_2').style.visibility='hidden'; 
+	document.getElementById('w31_3').style.visibility='hidden';
+	document.getElementById('w31_4').style.visibility='visible';
+}
+
+function isOneChecked32_1()
+{	document.getElementById('w32_1').style.visibility='visible';
+	document.getElementById('w32_2').style.visibility='hidden'; 
+	document.getElementById('w32_3').style.visibility='hidden';
+	document.getElementById('w32_4').style.visibility='hidden';
+}
+function isOneChecked32_2()
+{	document.getElementById('w32_1').style.visibility='hidden';
+	document.getElementById('w32_2').style.visibility='visible'; 
+	document.getElementById('w32_3').style.visibility='hidden';
+	document.getElementById('w32_4').style.visibility='hidden';
+}
+function isOneChecked32_3()
+{	document.getElementById('w32_1').style.visibility='hidden';
+	document.getElementById('w32_2').style.visibility='hidden'; 
+	document.getElementById('w32_3').style.visibility='visible';
+	document.getElementById('w32_4').style.visibility='hidden';
+}
+function isOneChecked32_4()
+{	document.getElementById('w32_1').style.visibility='hidden';
+	document.getElementById('w32_2').style.visibility='hidden'; 
+	document.getElementById('w32_3').style.visibility='hidden';
+	document.getElementById('w32_4').style.visibility='visible';
+}
+
+function isOneChecked33_1()
+{	document.getElementById('w33_1').style.visibility='visible';
+	document.getElementById('w33_2').style.visibility='hidden'; 
+	document.getElementById('w33_3').style.visibility='hidden';
+	document.getElementById('w33_4').style.visibility='hidden';
+}
+function isOneChecked33_2()
+{	document.getElementById('w33_1').style.visibility='hidden';
+	document.getElementById('w33_2').style.visibility='visible'; 
+	document.getElementById('w33_3').style.visibility='hidden';
+	document.getElementById('w33_4').style.visibility='hidden';
+}
+function isOneChecked33_3()
+{	document.getElementById('w33_1').style.visibility='hidden';
+	document.getElementById('w33_2').style.visibility='hidden'; 
+	document.getElementById('w33_3').style.visibility='visible';
+	document.getElementById('w33_4').style.visibility='hidden';
+}
+function isOneChecked33_4()
+{	document.getElementById('w33_1').style.visibility='hidden';
+	document.getElementById('w33_2').style.visibility='hidden'; 
+	document.getElementById('w33_3').style.visibility='hidden';
+	document.getElementById('w33_4').style.visibility='visible';
+}
+
+function isOneChecked34_1()
+{	document.getElementById('w34_1').style.visibility='visible';
+	document.getElementById('w34_2').style.visibility='hidden'; 
+	document.getElementById('w34_3').style.visibility='hidden';
+	document.getElementById('w34_4').style.visibility='hidden';
+}
+function isOneChecked34_2()
+{	document.getElementById('w34_1').style.visibility='hidden';
+	document.getElementById('w34_2').style.visibility='visible'; 
+	document.getElementById('w34_3').style.visibility='hidden';
+	document.getElementById('w34_4').style.visibility='hidden';
+}
+function isOneChecked34_3()
+{	document.getElementById('w34_1').style.visibility='hidden';
+	document.getElementById('w34_2').style.visibility='hidden'; 
+	document.getElementById('w34_3').style.visibility='visible';
+	document.getElementById('w34_4').style.visibility='hidden';
+}
+function isOneChecked34_4()
+{	document.getElementById('w34_1').style.visibility='hidden';
+	document.getElementById('w34_2').style.visibility='hidden'; 
+	document.getElementById('w34_3').style.visibility='hidden';
+	document.getElementById('w34_4').style.visibility='visible';
+}
+
+function isOneChecked35_1()
+{	document.getElementById('w35_1').style.visibility='visible';
+	document.getElementById('w35_2').style.visibility='hidden'; 
+	document.getElementById('w35_3').style.visibility='hidden';
+	document.getElementById('w35_4').style.visibility='hidden';
+}
+function isOneChecked35_2()
+{	document.getElementById('w35_1').style.visibility='hidden';
+	document.getElementById('w35_2').style.visibility='visible'; 
+	document.getElementById('w35_3').style.visibility='hidden';
+	document.getElementById('w35_4').style.visibility='hidden';
+}
+function isOneChecked35_3()
+{	document.getElementById('w35_1').style.visibility='hidden';
+	document.getElementById('w35_2').style.visibility='hidden'; 
+	document.getElementById('w35_3').style.visibility='visible';
+	document.getElementById('w35_4').style.visibility='hidden';
+}
+function isOneChecked35_4()
+{	document.getElementById('w35_1').style.visibility='hidden';
+	document.getElementById('w35_2').style.visibility='hidden'; 
+	document.getElementById('w35_3').style.visibility='hidden';
+	document.getElementById('w35_4').style.visibility='visible';
+}
+
+function isOneChecked36_1()
+{	document.getElementById('w36_1').style.visibility='visible';
+	document.getElementById('w36_2').style.visibility='hidden'; 
+	document.getElementById('w36_3').style.visibility='hidden';
+	document.getElementById('w36_4').style.visibility='hidden';
+}
+function isOneChecked36_2()
+{	document.getElementById('w36_1').style.visibility='hidden';
+	document.getElementById('w36_2').style.visibility='visible'; 
+	document.getElementById('w36_3').style.visibility='hidden';
+	document.getElementById('w36_4').style.visibility='hidden';
+}
+function isOneChecked36_3()
+{	document.getElementById('w36_1').style.visibility='hidden';
+	document.getElementById('w36_2').style.visibility='hidden'; 
+	document.getElementById('w36_3').style.visibility='visible';
+	document.getElementById('w36_4').style.visibility='hidden';
+}
+function isOneChecked36_4()
+{	document.getElementById('w36_1').style.visibility='hidden';
+	document.getElementById('w36_2').style.visibility='hidden'; 
+	document.getElementById('w36_3').style.visibility='hidden';
+	document.getElementById('w36_4').style.visibility='visible';
+}
+
+function isOneChecked37_1()
+{	document.getElementById('w37_1').style.visibility='visible';
+	document.getElementById('w37_2').style.visibility='hidden'; 
+	document.getElementById('w37_3').style.visibility='hidden';
+	document.getElementById('w37_4').style.visibility='hidden';
+}
+function isOneChecked37_2()
+{	document.getElementById('w37_1').style.visibility='hidden';
+	document.getElementById('w37_2').style.visibility='visible'; 
+	document.getElementById('w37_3').style.visibility='hidden';
+	document.getElementById('w37_4').style.visibility='hidden';
+}
+function isOneChecked37_3()
+{	document.getElementById('w37_1').style.visibility='hidden';
+	document.getElementById('w37_2').style.visibility='hidden'; 
+	document.getElementById('w37_3').style.visibility='visible';
+	document.getElementById('w37_4').style.visibility='hidden';
+}
+function isOneChecked37_4()
+{	document.getElementById('w37_1').style.visibility='hidden';
+	document.getElementById('w37_2').style.visibility='hidden'; 
+	document.getElementById('w37_3').style.visibility='hidden';
+	document.getElementById('w37_4').style.visibility='visible';
+}
